5f3c8c2e66046bee4c79b1052cf98164b779df34
ctirabassi <carlostirabassi@gmail.com>
2019-10-24 12:52:05 -0300

    first commit

---
+++ src/app/models/Students.js
@@ -0,0 +1,19 @@
+import { Model, Sequelize } from 'sequelize';
+
+class Students extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Students;
---
+++ src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+import Students from '../app/models/Students';
+
+const models = [User, Students];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
